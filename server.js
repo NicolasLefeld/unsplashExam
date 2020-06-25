5905bd2c11433867eed41e334518f2a6c3689854
NicolasLefeld <lefeldnicolas@gmail.com>
2020-06-25 18:30:33 -0300

    Remove cors

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 require('dotenv').config();
 const app = require('express')();
 const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const corsOptions = require('./utils/corsOptions');
 
 // Midlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.use(cors(corsOptions));
 
 // Routers
 app.use('/photo', require('./routers/photo'));
