c2ce2bb4e17cdfd85ef6ccfa43f2b549b0b1f673
NicolasLefeld <lefeldnicolas@gmail.com>
2020-06-25 18:36:20 -0300

    Added postman doc to readme and try catch to the router

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Development start at: 17:23h
 Development ends at: 18:26h 
+
+Public repository: https://github.com/NicolasLefeld/unsplashExam
+
+Postman doc: https://www.getpostman.com/collections/e2cd0cd38bb6bb693527
--- routers/photo.js
+++ routers/photo.js
@@ -7,21 +7,26 @@ router
 
 //  -- GET ALL PROVIDERS --
 
-  .get('/:phrase', async (req, res) => {
-    const { phrase } = req.params;
-    const { page, perPage, filters } = req.body;
-    const result = await searchPhoto(phrase, page, perPage, filters);
-    if (result.total > 0) {
-      return res.status(200).json({
-        success: true,
-        message: 'Photos found',
-        body: result,
+  .get('/:phrase', async (req, res, next) => {
+    try {
+      const { phrase } = req.params;
+      const { page, perPage, filters } = req.body;
+      const result = await searchPhoto(phrase, page, perPage, filters);
+      if (result.total > 0) {
+        return res.status(200).json({
+          success: true,
+          message: 'Photos found',
+          body: result,
+        });
+      }
+      return res.status(500).json({
+        success: false,
+        message: `Any photo found for ${phrase}`,
       });
+    } catch (error) {
+      console.error(`🔥 An error ocurred: ${err}`);
+      next('An error occurred trying to process your request');
     }
-    return res.status(500).json({
-      success: false,
-      message: `Any photo found for ${phrase}`,
-    });
   });
 
 module.exports = router;
